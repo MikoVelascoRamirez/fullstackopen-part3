c627c7890dde3ac7e9d75804254bb254ff36e223
Miko Velasco Ramirez <launchx08269@innovaccion.mx>
2024-03-06 16:43:17 -0600

    8. setting initial port.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require("cors");
 const morgan = require("morgan");
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static('dist'));
 
