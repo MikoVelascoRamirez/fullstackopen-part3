14c8f11f91b8d1b766259bb6fc011cfbcbe9b94f
Miko Velasco Ramirez <launchx08269@innovaccion.mx>
2024-03-05 22:01:25 -0600

    4. feat: installing and using cors module to allowing cross site origin.

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const morgan = require("morgan");
 
 const PORT = 3001;
+
+app.use(cors());
 
 app.use(express.json());
 app.use(morgan('tiny', {
