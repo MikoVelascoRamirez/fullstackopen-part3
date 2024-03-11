cf06fe074145adcdce9355fb60bddab974ac2b65
Miko Velasco Ramirez <launchx08269@innovaccion.mx>
2024-03-11 16:01:17 -0600

    15. encapsulating db connection string into a secret env variable.

--- mongo.js
+++ mongo.js
@@ -1,12 +1,7 @@
+require('dotenv').config();
 const mongoose = require("mongoose");
 
-if(process.argv.length < 3){
-    console.log("Please, provide a password")
-    process.exit(1);
-}
-
-const password = process.argv[2];
-const url = `mongodb+srv://miko_o:${password}@cluster0.kachhpe.mongodb.net/phonebook?retryWrites=true&w=majority&appName=Cluster0`;
+const url = process.env.MONGODB_URI;
 
 mongoose.set('strictQuery', false);
 mongoose.connect(url);
