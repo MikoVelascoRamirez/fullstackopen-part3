4fec2a5f38787d793317cc35923e6ab1e2fe2c5e
Miko Velasco Ramirez <launchx08269@innovaccion.mx>
2024-03-11 16:04:17 -0600

    16. writting Contact model configuration.

---
+++ Models/Contact.js
@@ -0,0 +1,25 @@
+// require('dotenv').config();
+const mongoose = require("mongoose");
+
+const url = process.env.MONGODB_URI;
+
+mongoose.set('strictQuery', false);
+mongoose.connect(url)
+    .then(() => console.log("connection to db sucessfull"))
+    .catch(err => console.log('error connecting to mongo db', err.message));
+
+const contactSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+contactSchema.set('toJSON', {
+    transform: (doc, returnedObj) => {
+        returnedObj.id = returnedObj._id.toString();
+
+        delete returnedObj._id;
+        delete returnedObj.__v;
+    }
+});
+
+module.exports = new mongoose.model('Contact', contactSchema);
