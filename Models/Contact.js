f03545e226fbbe7bbed573500a06b31cff428c30
Miko Velasco Ramirez <launchx08269@innovaccion.mx>
2024-03-21 14:17:35 -0600

    32. fix: implementing number's contact validator.

--- Models/Contact.js
+++ Models/Contact.js
@@ -14,7 +14,16 @@ const contactSchema = new mongoose.Schema({
         minLength: 3,
         required: true
     },
-    number: String
+    number: {
+        type: String,
+        minLength: 8,
+        validate: {
+            validator: function(val){
+                return /(?<!.)\b(\d{2,3}){1}-(\d+)\b(?!.)/g.test(val);
+            }
+        },
+        required: true
+    }
 })
 
 contactSchema.set('toJSON', {
